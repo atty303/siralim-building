842142b80d9cc0b5fc9a4a6be80972d6b9de0eba
Koji AGAWA <atty303@gmail.com>
2023-02-12 16:32:55 +0900

    feat: json

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+
 use dioxus::prelude::*;
 use rust_embed::RustEmbed;
 use log::{LevelFilter};
@@ -11,9 +12,11 @@ fn main() {
 fn App(cx: Scope) -> Element {
     let data = Data::get("data/creatures.json").unwrap();
     let str = std::str::from_utf8(data.data.as_ref()).unwrap();
+    let creatures: Vec<Creature> = serde_json::from_slice(data.data.as_ref()).unwrap();
     cx.render(rsx! {
-        div { "Hello, world! d22" }
-        div { str }
+        div {
+            creatures.iter().map(|c| rsx!( p { "{c.creature}" }))
+        }
     })
 }
 
@@ -21,3 +24,10 @@ fn App(cx: Scope) -> Element {
 #[folder = "src/data/"]
 #[prefix = "data/"]
 struct Data;
+
+#[derive(serde::Deserialize)]
+struct Creature {
+    class: String,
+    family: String,
+    creature: String,
+}
