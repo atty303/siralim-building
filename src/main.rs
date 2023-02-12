f43c666705b55dc68972b8463519314d6d598ef7
Koji AGAWA <atty303@gmail.com>
2023-02-12 12:46:12 +0900

    feat:

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,23 @@
 #![allow(non_snake_case)]
 use dioxus::prelude::*;
+use rust_embed::RustEmbed;
+use log::{LevelFilter};
 
 fn main() {
+    dioxus_logger::init(log::LevelFilter::Debug).expect("failed to init logger");
     dioxus_web::launch(App);
 }
 
 fn App(cx: Scope) -> Element {
+    let data = Data::get("data/creatures.json").unwrap();
+    let str = std::str::from_utf8(data.data.as_ref()).unwrap();
     cx.render(rsx! {
         div { "Hello, world! d22" }
+        div { str }
     })
 }
+
+#[derive(RustEmbed)]
+#[folder = "src/data/"]
+#[prefix = "data/"]
+struct Data;
