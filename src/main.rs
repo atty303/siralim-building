d998a763be1894b1771db467735b2cddc22f4d44
Koji AGAWA <atty303@gmail.com>
2023-02-13 06:25:03 +0900

    feat: wip

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,11 @@
 mod components;
 mod creatures;
 mod data;
+mod party;
 
-use crate::components::ClassIcon;
 use crate::creatures::CreatureModal;
-use crate::data::Creature;
+use crate::party::PartyMember;
 use dioxus::prelude::*;
-use dioxus_free_icons::icons::bs_icons;
-use dioxus_free_icons::Icon;
 
 fn main() {
     dioxus_logger::init(log::LevelFilter::Debug).expect("failed to init logger");
@@ -42,99 +40,5 @@ fn App(cx: Scope) -> Element {
                 show_creatures_modal.set(false);
             }
         }
-        // Modal {
-        //     show: **show_creatures_modal,
-        //     on_request_close: move |_| show_creatures_modal.set(false),
-        //     input { }
-        //     CreatureTable {
-        //         items: creatures
-        //         on_select: move |e|
-        //     }
-        // }
     })
-}
-
-#[derive(Props, PartialEq)]
-struct PartyMemberProps {
-    creature: (Option<Creature>, Option<Creature>, Option<Creature>),
-}
-
-fn PartyMember(cx: Scope<PartyMemberProps>) -> Element {
-    cx.render(rsx! {
-        div {
-            class: "party-member",
-            div {
-                class: "left-pane",
-                "x"
-            }
-            div {
-                class: "right-pane",
-                ul {
-                    li {
-                        PartyMemberTrait {
-                            creature: (&cx.props.creature.0,)
-                            empty_text: "Click to add primary trait",
-                        }
-                    }
-                    li {
-                        PartyMemberTrait {
-                            creature: (&cx.props.creature.1,)
-                            empty_text: "Click to add fused trait",
-                        }
-                    }
-                    li {
-                        PartyMemberTrait {
-                            creature: (&cx.props.creature.2,)
-                            empty_text: "Click to add artifact trait",
-                        }
-                    }
-                }
-            }
-        }
-    })
-}
-
-#[derive(Props, PartialEq)]
-struct PartyMemberTraitProps<'a> {
-    creature: (&'a Option<Creature>,),
-    empty_text: &'static str,
-}
-
-fn PartyMemberTrait<'a>(cx: Scope<'a, PartyMemberTraitProps<'a>>) -> Element {
-    let e = if let Some(c) = &cx.props.creature.0 {
-        rsx! {
-            div {
-                class: "detail non-empty",
-                div {
-                    class: "creature",
-                    span { ClassIcon { value: &c.class } c.creature.as_str() }
-                }
-                div {
-                    class: "trait-name",
-                    span { c.trait_name.as_str() }
-                }
-                div {
-                    class: "trait-description",
-                    span { c.trait_description.as_str() }
-                }
-            }
-            div {
-                class: "clear",
-                button {
-                    Icon { width: 24, height: 24, icon: bs_icons::BsXLg }
-                }
-            }
-        }
-    } else {
-        rsx! {
-            div {
-                class: "detail empty",
-                cx.props.empty_text
-            }
-            div {
-                class: "clear"
-            }
-        }
-    };
-    cx.render(e)
 }
