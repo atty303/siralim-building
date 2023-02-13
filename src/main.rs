fa1ae7ef394a2831e5e2de4656dc0738f3d136bf
Koji AGAWA <atty303@gmail.com>
2023-02-13 22:14:59 +0900

    feat: wip

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,10 @@ mod party;
 
 use crate::creatures::CreatureModal;
 use crate::data::Creature;
-use crate::party::{Member, Party};
+use crate::party::{Member, Party, PartyMemberSwapEvent};
 use dioxus::prelude::*;
 use dioxus_web::Config;
+use std::borrow::BorrowMut;
 
 fn main() {
     dioxus_logger::init(log::LevelFilter::Debug).expect("failed to init logger");
@@ -29,18 +30,20 @@ struct AppProps {
 
 fn App(cx: Scope<AppProps>) -> Element {
     let show_creatures_modal = use_state(cx, || false);
-    let party = vec![
-        Member {
-            primary_creature: Some(cx.props.creatures.get(0).unwrap().clone()),
-            fused_creature: None,
-            artifact_creature: None,
-        },
-        Member {
-            primary_creature: Some(cx.props.creatures.get(100).unwrap().clone()),
-            fused_creature: None,
-            artifact_creature: None,
-        },
-    ];
+    let party = use_state(cx, || {
+        vec![
+            Member {
+                primary_creature: Some(cx.props.creatures.get(0).unwrap().clone()),
+                fused_creature: None,
+                artifact_creature: None,
+            },
+            Member {
+                primary_creature: Some(cx.props.creatures.get(100).unwrap().clone()),
+                fused_creature: None,
+                artifact_creature: None,
+            },
+        ]
+    });
 
     cx.render(rsx! {
         div {
@@ -50,7 +53,25 @@ fn App(cx: Scope<AppProps>) -> Element {
                 "open"
             }
             Party {
-                party: party,
+                party: party
+                on_swap: move |e: PartyMemberSwapEvent| {
+                    log::info!("{:?}", e);
+                    let from_member = party.get().get(e.from_position as usize).unwrap().clone();
+                    let from = from_member.get_creature(e.from_index);
+                    let to_member = party.get().get(e.to_position as usize).unwrap().clone();
+                    let to = to_member.get_creature(e.to_index);
+
+                    let mut f = from_member.clone();
+                    f.set_creature(e.from_index, &to);
+                    let mut t = to_member.clone();
+                    t.set_creature(e.to_index, &from);
+
+                    let mut p = party.get().to_vec();
+                    p[e.from_position as usize] = t;
+                    p[e.to_position as usize] = f;
+
+                    party.set(p);
+                }
             }
         }
         CreatureModal {
--- src/party.rs
+++ src/party.rs
@@ -12,20 +12,64 @@ pub struct Member {
     pub fused_creature: Option<Creature>,
     pub artifact_creature: Option<Creature>,
 }
-
-#[derive(PartialEq, Props)]
-pub struct PartyProps {
-    party: Vec<Member>,
+impl Member {
+    pub fn get_creature(&self, i: i32) -> &Option<Creature> {
+        match i {
+            0 => &self.primary_creature,
+            1 => &self.fused_creature,
+            2 => &self.artifact_creature,
+            _ => &None,
+        }
+    }
+    pub fn set_creature(&mut self, i: i32, c: &Option<Creature>) {
+        match i {
+            0 => self.primary_creature = c.clone(),
+            1 => self.fused_creature = c.clone(),
+            2 => self.artifact_creature = c.clone(),
+            _ => (),
+        }
+    }
 }
 
-pub fn Party(cx: Scope<PartyProps>) -> Element {
+#[derive(Debug)]
+pub struct PartyMemberSwapEvent {
+    pub from_position: i32,
+    pub from_index: i32,
+    pub to_position: i32,
+    pub to_index: i32,
+}
+
+#[derive(Props)]
+pub struct PartyProps<'a> {
+    party: &'a Vec<Member>,
+    on_swap: EventHandler<'a, PartyMemberSwapEvent>,
+}
+
+pub fn Party<'a>(cx: Scope<'a, PartyProps<'a>>) -> Element {
+    let dragging: &UseState<Option<PartyMemberTraitDragEvent>> = use_state(cx, || None);
+
     cx.render(rsx! {
         div {
             class: "party",
-            cx.props.party.iter().map(|m| {
+            cx.props.party.iter().enumerate().map(|(i, m)| {
                 rsx! {
                     PartyMember {
+                        position: i as i32
                         creature: (m.primary_creature.clone(), m.fused_creature.clone(), m.artifact_creature.clone())
+                        on_drag_start: move |e| {
+                            dragging.set(Some(e));
+                        }
+                        on_drop: move |e: PartyMemberTraitDropEvent| {
+                            if let Some(a) = dragging.get() {
+                                cx.props.on_swap.call(PartyMemberSwapEvent {
+                                    from_position: a.position,
+                                    from_index: a.index,
+                                    to_position: e.position,
+                                    to_index: e.index,
+                                })
+                            }
+                            dragging.set(None);
+                        }
                     }
                 }
             })
@@ -33,12 +77,26 @@ pub fn Party(cx: Scope<PartyProps>) -> Element {
     })
 }
 
-#[derive(PartialEq, Props)]
-pub struct PartyMemberProps {
-    creature: (Option<Creature>, Option<Creature>, Option<Creature>),
+struct PartyMemberTraitDragEvent {
+    position: i32,
+    index: i32,
+    creature: Creature,
 }
 
-pub fn PartyMember(cx: Scope<PartyMemberProps>) -> Element {
+struct PartyMemberTraitDropEvent {
+    position: i32,
+    index: i32,
+}
+
+#[derive(Props)]
+pub struct PartyMemberProps<'a> {
+    position: i32,
+    creature: (Option<Creature>, Option<Creature>, Option<Creature>),
+    on_drag_start: EventHandler<'a, PartyMemberTraitDragEvent>,
+    on_drop: EventHandler<'a, PartyMemberTraitDropEvent>,
+}
+
+pub fn PartyMember<'a>(cx: Scope<'a, PartyMemberProps<'a>>) -> Element {
     cx.render(rsx! {
         div {
             class: "party-member",
@@ -52,19 +110,46 @@ pub fn PartyMember(cx: Scope<PartyMemberProps>) -> Element {
                     li {
                         PartyMemberTrait {
                             creature: (&cx.props.creature.0,)
-                            empty_text: "Click to add primary trait",
+                            empty_text: "Click to add primary trait"
+                            on_drag_start: move |e| cx.props.on_drag_start.call(PartyMemberTraitDragEvent {
+                                position: cx.props.position,
+                                index: 0,
+                                creature: e,
+                            })
+                            on_drop: move |_| cx.props.on_drop.call(PartyMemberTraitDropEvent {
+                                position: cx.props.position,
+                                index: 0,
+                            })
                         }
                     }
                     li {
                         PartyMemberTrait {
                             creature: (&cx.props.creature.1,)
-                            empty_text: "Click to add fused trait",
+                            empty_text: "Click to add fused trait"
+                            on_drag_start: move |e| cx.props.on_drag_start.call(PartyMemberTraitDragEvent {
+                                position: cx.props.position,
+                                index: 1,
+                                creature: e,
+                            })
+                            on_drop: move |_| cx.props.on_drop.call(PartyMemberTraitDropEvent {
+                                position: cx.props.position,
+                                index: 0,
+                            })
                         }
                     }
                     li {
                         PartyMemberTrait {
                             creature: (&cx.props.creature.2,)
-                            empty_text: "Click to add artifact trait",
+                            empty_text: "Click to add artifact trait"
+                            on_drag_start: move |e| cx.props.on_drag_start.call(PartyMemberTraitDragEvent {
+                                position: cx.props.position,
+                                index: 2,
+                                creature: e,
+                            })
+                            on_drop: move |_| cx.props.on_drop.call(PartyMemberTraitDropEvent {
+                                position: cx.props.position,
+                                index: 0,
+                            })
                         }
                     }
                 }
@@ -73,10 +158,12 @@ pub fn PartyMember(cx: Scope<PartyMemberProps>) -> Element {
     })
 }
 
-#[derive(Props, PartialEq)]
+#[derive(Props)]
 struct PartyMemberTraitProps<'a> {
     creature: (&'a Option<Creature>,),
     empty_text: &'static str,
+    on_drag_start: EventHandler<'a, Creature>,
+    on_drop: EventHandler<'a>,
 }
 
 fn PartyMemberTrait<'a>(cx: Scope<'a, PartyMemberTraitProps<'a>>) -> Element {
@@ -87,8 +174,19 @@ fn PartyMemberTrait<'a>(cx: Scope<'a, PartyMemberTraitProps<'a>>) -> Element {
                 class: "trait non-empty",
                 "draggable": "true",
                 style: "opacity: {opacity}",
-                ondragstart: move |_| opacity.set(0.5),
+                onclick: move |e| {
+                    log::debug!("onclick: {:?}", e);
+                },
+                ondragstart: move |e| {
+                    log::debug!("ondragstart: {:?}", e);
+                    opacity.set(0.5);
+                    cx.props.on_drag_start.call(c.clone());
+                },
                 ondragend: move |_| opacity.set(1.0),
+                ondrop: move |e| {
+                    log::debug!("ondrop: {:?}", e);
+                    cx.props.on_drop.call(());
+                },
                 div {
                     class: "creature",
                     span { ClassIcon { value: &c.class } c.creature.as_str() }
