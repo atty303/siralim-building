47f2375d8a7ddbd6de6eb6ab0c14fab468d5ce8d
Koji AGAWA <atty303@gmail.com>
2023-02-16 16:07:03 +0900

    feat: gen

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -14,7 +14,23 @@ struct Record {
     material_name: String,
 }
 
-fn main() {
+#[derive(Debug, Deserialize)]
+struct CreatureRecord {
+    battle_sprite: String,
+    name: String,
+    race: String,
+    klass: String,
+    health: i32,
+    attack: i32,
+    intelligence: i32,
+    defense: i32,
+    speed: i32,
+    total: i32,
+    sources: String,
+    r#trait: String,
+}
+
+fn compendium_traits() {
     let rep = reqwest::blocking::get("https://docs.google.com/spreadsheets/d/1qvWwf1fNB5jN8bJ8dFGAVzC7scgDCoBO-hglwjTT4iY/gviz/tq?tqx=out:csv&sheet=Traits").unwrap();
     let body = rep.bytes().unwrap();
     //let text = rep.text().unwrap();
@@ -29,8 +45,29 @@ fn main() {
         "trait_description",
         "material_name",
     ]));
+    let traits = reader
+        .deserialize()
+        .skip(2)
+        .map(|r| r.unwrap())
+        .collect::<Vec<Record>>();
     for maybe_row in reader.deserialize().skip(2) {
         let row: Record = maybe_row.unwrap();
         println!("{:?}", row);
     }
+}
+
+fn load_creatures() -> Vec<CreatureRecord> {
+    let mut reader = csv::ReaderBuilder::new()
+        .has_headers(true)
+        .from_path("siralim-ultimate-api/app/data/creatures.csv")
+        .unwrap();
+    reader
+        .deserialize()
+        .map(|r| r.unwrap())
+        .collect::<Vec<CreatureRecord>>()
+}
+
+fn main() {
+    let creatures = load_creatures();
+    println!("{:?}", creatures);
 }
