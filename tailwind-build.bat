77a812467e4fa59c2da899fd6b39f53dd982f33d
Koji AGAWA <atty303@gmail.com>
2023-03-04 01:43:21 +0900

    chore: remove tailwind

--- tailwind-build.bat
+++
@@ -1,3 +0,0 @@
-:; tailwindcss -i public/tailwind.css -o "$TRUNK_STAGING_DIR/tailwind.css"; exit $?
-@ECHO OFF
-tailwindcss-windows-x64.exe -i public\tailwind.css -o %TRUNK_STAGING_DIR%\tailwind.css
