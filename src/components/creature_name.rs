da136e375e41adb960b3208a80e738f468422843
Koji AGAWA <atty303@gmail.com>
2023-03-04 18:13:27 +0900

    refactor: wip

--- src/components/creature_name.rs
+++ src/components/creature_name.rs
@@ -3,6 +3,7 @@ use yew::prelude::*;
 use data::r#trait::Trait;
 
 use crate::components::icon::ClassIcon;
+use crate::components::tooltip::Tooltip;
 
 #[derive(Properties, PartialEq)]
 pub struct CreatureNameProps {
@@ -13,40 +14,44 @@ pub struct CreatureNameProps {
 #[function_component(CreatureName)]
 pub fn creature_name(props: &CreatureNameProps) -> Html {
     let c = &props.r#trait;
-    html! {
-        <div class="creature-name tooltip">
-            <div class="tooltip-text">
-                {if let Some(sprite) = &c.sprite {
-                    html! { <img class="sprite" src={format!("battle_sprites/{}", sprite)} /> }
-                } else {
-                    html! { }
-                }}
-                {if let Some(stats) = &c.stats {
-                    html! {
-                        <ul class="stats">
-                            <li class="health"><img src="image/health.png" />{format!("{}", stats.health)}</li>
-                            <li class="attack"><img src="image/attack.png" />{format!("{}", stats.attack)}</li>
-                            <li class="intelligence"><img src="image/intelligence.png" />{format!("{}", stats.intelligence)}</li>
-                            <li class="defense"><img src="image/defense.png" />{format!("{}", stats.defense)}</li>
-                            <li class="speed"><img src="image/speed.png" />{format!("{}", stats.speed)}</li>
-                        </ul>
-                    }
-                } else {
-                    html! { }
-                }}
-                <div>
-                    <span class="creature"><ClassIcon value={c.class.clone()} />{c.class.as_str()}{": "}{c.creature.as_str()}</span><br/>
-                    <span class="family"><span>{"Family: "}</span>{c.family.as_str()}</span><br/>
-                    <span class="trait"><span>{"Trait: "}</span>{c.trait_name.as_str()}</span><br/>
-                    <span class="material"><span>{"Material: "}</span>{c.material_name.as_str()}</span><br/>
-                    <span class="sources"><span>{"Sources:"}</span>
-                        <ul>
-                            {c.sources.iter().map(|s| html! { <li>{s}</li> }).collect::<Html>()}
-                        </ul>
-                    </span>
-                </div>
+
+    let tooltip = html! {
+        <div class="creature-name-tooltip">
+            {if let Some(sprite) = &c.sprite {
+                html! { <img class="sprite" src={format!("battle_sprites/{}", sprite)} /> }
+            } else {
+                html! { }
+            }}
+            {if let Some(stats) = &c.stats {
+                html! {
+                    <ul class="creature-name-tooltip__stats">
+                        <li class="health"><img src="image/health.png" />{format!("{}", stats.health)}</li>
+                        <li class="attack"><img src="image/attack.png" />{format!("{}", stats.attack)}</li>
+                        <li class="intelligence"><img src="image/intelligence.png" />{format!("{}", stats.intelligence)}</li>
+                        <li class="defense"><img src="image/defense.png" />{format!("{}", stats.defense)}</li>
+                        <li class="speed"><img src="image/speed.png" />{format!("{}", stats.speed)}</li>
+                    </ul>
+                }
+            } else {
+                html! { }
+            }}
+            <div>
+                <span class="creature-name-tooltip__creature"><ClassIcon value={c.class.clone()} />{c.class.as_str()}{": "}{c.creature.as_str()}</span><br/>
+                <span class="creature-name-tooltip__family"><span>{"Family: "}</span>{c.family.as_str()}</span><br/>
+                <span class="creature-name-tooltip__trait"><span>{"Trait: "}</span>{c.trait_name.as_str()}</span><br/>
+                <span class="creature-name-tooltip__material"><span>{"Material: "}</span>{c.material_name.as_str()}</span><br/>
+                <span class="creature-name-tooltip__sources"><span>{"Sources:"}</span>
+                    <ul>
+                        {c.sources.iter().map(|s| html! { <li>{s}</li> }).collect::<Html>()}
+                    </ul>
+                </span>
             </div>
-            <span>
+        </div>
+    };
+
+    html! {
+        <Tooltip {tooltip}>
+            <span class="name-plate">
                 {if props.icon {
                     html! { <ClassIcon value={c.class.clone()} /> }
                 } else {
@@ -54,6 +59,6 @@ pub fn creature_name(props: &CreatureNameProps) -> Html {
                 }}
                 {c.creature.as_str()}
             </span>
-        </div>
+        </Tooltip>
     }
 }
