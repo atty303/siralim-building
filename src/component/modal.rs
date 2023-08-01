fd49175a85d643eb31d0fe49d93fd9979d78bc47
Koji AGAWA <atty303@gmail.com>
2023-08-02 06:45:12 +0900

    feat: use_modal

--- src/component/modal.rs
+++ src/component/modal.rs
@@ -7,33 +7,6 @@ use wasm_bindgen::JsCast;
 
 use crate::component::outline_icon::OutlineIcon;
 
-//#[inline_props]
-// pub fn Modal<'a>(cx: Scope<'a>, modal: &'a mut UseModal, children: Element<'a>) -> Element<'a> {
-//     render! {
-//         dialog {
-//             class: "modal",
-//             onmounted: move |e| {
-//                 modal.deref_mut().onmount(e);
-//             },
-//             form {
-//                 method: "dialog",
-//                 class: "modal-box max-w-full h-full relative",
-//                 style: "width: calc(100vw - 5em)",
-//                 button {
-//                     class: "btn btn-sm btn-circle btn-ghost absolute right-1 top-1",
-//                     onclick: move |_| {
-//                         modal.close();
-//                     },
-//                     OutlineIcon {
-//                         icon: Shape::XMark,
-//                     }
-//                 }
-//                 &cx.props.children
-//             }
-//         }
-//     }
-// }
-
 pub fn use_modal(cx: &ScopeState) -> &ModalState {
     let modalRef: &UseRef<Option<web_sys::HtmlDialogElement>> = use_ref(cx, || None);
 
@@ -106,54 +79,9 @@ impl ModalState {
         };
     }
 
-    //     pub fn modal<'a>(&self, _cx: &'a ScopeState) -> fn(Scope<'a, ModalProps<'a>>) -> Element<'a> {
-    //         move |cx| {
-    //             render! {
-    //                 dialog {
-    //                     class: "modal modal-open",
-    //                     onmounted: move |e| {
-    //                         log::debug!("{:?}", e.get_raw_element().unwrap().downcast_ref::<web_sys::Element>());
-    //                         let el = e
-    //                         .get_raw_element()
-    //                         .expect("expecting raw element")
-    //                         .downcast_ref::<web_sys::Element>()
-    //                         .expect("expecting Element")
-    //                         .dyn_ref::<web_sys::HtmlDialogElement>()
-    //                         .expect("expecting HtmlDialogElement");
-    //                         log::debug!("{:?}", el);
-    //
-    //                         // cx.props.modalRef.write().replace(el.clone());
-    //                         // self.modalRef.write().replace(el.clone());
-    //                     },
-    //                     form {
-    //                         method: "dialog",
-    //                         class: "modal-box max-w-full h-full relative",
-    //                         style: "width: calc(100vw - 5em)",
-    //                         button {
-    //                             class: "btn btn-sm btn-circle btn-ghost absolute right-1 top-1",
-    //                             onclick: move |_| {
-    //                                 // self.close();
-    //                             },
-    //                             OutlineIcon {
-    //                                 icon: Shape::XMark,
-    //                             }
-    //                         }
-    //                         &cx.props.children
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //     }
-    //
-    //     pub fn show(&self) {
-    //         if let Some(el) = self.modalRef.read().as_ref() {
-    //             el.show();
-    //         };
-    //     }
-    //
-    //     pub fn close(&self) {
-    //         if let Some(el) = self.modalRef.read().as_ref() {
-    //             el.close();
-    //         };
-    //     }
+    pub fn close(&self) {
+        if let Some(el) = self.modalRef.read().as_ref() {
+            el.close();
+        };
+    }
 }
