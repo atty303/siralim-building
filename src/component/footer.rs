4eabd59515d09e5a84bdd7bb47b11f6b288c4a9e
Koji AGAWA <atty303@gmail.com>
2023-08-06 09:37:59 +0900

    feat: footer

--- src/component/footer.rs
+++ src/component/footer.rs
@@ -5,20 +5,34 @@ use dioxus::prelude::*;
 pub fn Footer(cx: Scope) -> Element {
     render! {
         footer {
-            class: "bg-neutral text-neutral-content mt-4 p-4",
-            "2023 Created by atty303. This site is not affiliated with Thylacine Studios."
-            br {}
-            "Data sourced from the"
-            a {
-                href: "https://docs.google.com/spreadsheets/d/1qvWwf1fNB5jN8bJ8dFGAVzC7scgDCoBO-hglwjTT4iY/edit#gid=0",
-                "Siralim Ultimate Compendium"
+            class: "footer p-4 mt-8 bg-neutral text-neutral-content",
+            div {
+                p {
+                    "2023 Created by "
+                    a {
+                        class: "link",
+                        href: "https://twitter.com/atty303",
+                        "atty303"
+                    }
+
+                    ". This site is not affiliated with Thylacine Studios."
+                }
+                p {
+                    "Data sourced from the "
+                    a {
+                        class: "link",
+                        href: "https://docs.google.com/spreadsheets/d/1qvWwf1fNB5jN8bJ8dFGAVzC7scgDCoBO-hglwjTT4iY/edit#gid=0",
+                        "Siralim Ultimate Compendium"
+                    }
+                    " and "
+                    a {
+                        class: "link",
+                        href: "https://github.com/rovermicrover/siralim-ultimate-api",
+                        "Siralim Ultimate API"
+                    }
+                    "."
+                }
             }
-            " and "
-            a {
-                href: "https://github.com/rovermicrover/siralim-ultimate-api",
-                "Siralim Ultimate API"
-            }
-            "."
         }
     }
 }
