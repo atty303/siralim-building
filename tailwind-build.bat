b7e2f54e6fe1d2af1221fb9f8941716bbe730631
Koji AGAWA <atty303@gmail.com>
2023-08-06 18:30:07 +0900

    ci: tailwind minify

--- tailwind-build.bat
+++ tailwind-build.bat
@@ -1,3 +1,3 @@
-:; npx tailwindcss -i src/input.css -o "$TRUNK_STAGING_DIR/tailwind.css"; exit $?
+:; npx tailwindcss -m -i src/input.css -o "$TRUNK_STAGING_DIR/tailwind.css"; exit $?
 @ECHO OFF
-npx tailwindcss -i src\input.css -o %TRUNK_STAGING_DIR%\tailwind.css
+npx tailwindcss -m -i src\input.css -o %TRUNK_STAGING_DIR%\tailwind.css
